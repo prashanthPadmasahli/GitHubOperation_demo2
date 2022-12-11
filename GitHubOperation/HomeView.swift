69570b66e5b90054ee9c8826ae7e8f2d1dd3515a
Prashanth <prashanthkumarshettigar@gamil.com>
2022-12-11 10:20:19 +0530

    home view added

---
+++ GitHubOperation/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  GitHubOperation
+//
+//  Created by Mac on 11/12/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
