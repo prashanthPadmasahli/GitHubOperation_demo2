a0480ededd490f1599ba1bb35f659e3a7c9f4101
Prashanth <prashanthkumarshettigar@gamil.com>
2022-12-11 10:11:34 +0530

    Added vstack

--- GitHubOperation/ContentView.swift
+++ GitHubOperation/ContentView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello")
+        VStack {
+            Text("Hello")
+            Text("Hello1")
+        }
+        
+        
     }
 }
 
