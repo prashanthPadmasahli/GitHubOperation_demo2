0d88747f0a84cc225310e3248dc9d578b36ea254
Prashanth <prashanthkumarshettigar@gamil.com>
2022-12-11 10:07:57 +0530

    Initial Commit

---
+++ GitHubOperation/GitHubOperationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubOperationApp.swift
+//  GitHubOperation
+//
+//  Created by Mac on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubOperationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
